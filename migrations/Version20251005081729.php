4df041f99945af7865dd4ce3a13f5be0bf9eaa77
Jochaz <jordan.chariot@gmail.com>
2025-10-05 10:18:17 +0200

    Reset migration

--- migrations/Version20251005081729.php
+++ migrations/Version20251005081729.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240802122653 extends AbstractMigration
+final class Version20251005081729 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20240802122653 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE devis ADD is_sous_totaux TINYINT(1) DEFAULT NULL');
-        $this->addSql('ALTER TABLE facture ADD is_sous_totaux TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE devis ADD duree_validite INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE devis DROP is_sous_totaux');
-        $this->addSql('ALTER TABLE facture DROP is_sous_totaux');
+        $this->addSql('ALTER TABLE devis DROP duree_validite');
     }
 }
